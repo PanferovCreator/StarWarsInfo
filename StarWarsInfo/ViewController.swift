b61c27746abf6eb646a7bf236fcac13386ace0ec
Panferov.creator <panferov.creator@gmail.com>
2023-05-05 19:13:40 +0500

    Initial Commit

---
+++ StarWarsInfo/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  StarWarsInfo
+//
+//  Created by Dmitriy Panferov on 05/05/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
