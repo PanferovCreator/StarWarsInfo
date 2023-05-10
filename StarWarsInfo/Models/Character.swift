42e846869b1c5f00d20fe2cb1ac25a8cbedb0ce4
Panferov.creator <panferov.creator@gmail.com>
2023-05-10 18:17:10 +0500

    Create Models

--- StarWarsInfo/Models/Character.swift
+++ StarWarsInfo/Models/Character.swift
@@ -8,6 +8,7 @@ import Foundation
 
 struct Characters: Decodable {
     let count: Int
+    let next: URL
     let results: [Character]
 }
 
@@ -15,46 +16,92 @@ struct Character: Decodable {
     let name: String
     let height: String
     let mass: String
-    let hair_color: String
-    let skin_color: String
-    let eye_color: String
-    let birth_year: String
+    let hairColor: String
+    let skinColor: String
+    let eyeColor: String
+    let birthYear: String
     let gender: String
+    let homeworld: URL
+    let starships: [URL]
     let url: URL
-}
-
-enum Person {
-    case lukeSkywalker
-    case darthVader
-    case obiWanKenobi
     
-    var url: URL {
-        switch self {
-            
-        case .lukeSkywalker:
-            return URL(string: "https://swapi.dev/api/people/1/")!
-        case .darthVader:
-            return URL(string: "https://swapi.dev/api/people/4/")!
-        case .obiWanKenobi:
-            return URL(string: "https://swapi.dev/api/people/10/")!
-        }
+    var description: String {
+        """
+        gender: \(gender)
+        height: \(height)
+        mass: \(mass)
+        hair color: \(hairColor)
+        skin color: \(skinColor)
+        eye color: \(eyeColor)
+        birth year: \(birthYear)
+        """
     }
 }
 
-enum Link {
-    case characters
-    case starShips
-    case planets
+struct Ships: Decodable {
+    let count: Int
+    let results: [Ship]
+}
+
+struct Ship: Decodable {
+    let name: String
+    let model: String
+    let manufacturer: String
+    let costInCredits: String
+    let length: String
+    let maxAtmospheringSpeed: String
+    let hyperdriveRating: String
+    let starshipClass: String
+    let crew: String
+    let passengers: String
+    let cargoCapacity: String
+    let url: URL
     
-    var url: URL {
-        switch self {
-            
-        case .characters:
-            return URL(string: "https://swapi.dev/api/people/")!
-        case .starShips:
-            return URL(string: "https://swapi.dev/api/starships/")!
-        case .planets:
-            return URL(string: "https://swapi.dev/api/planets/")!
-        }
+    var description: String {
+        """
+        name: \(name)
+        model: \(model)
+        manufacturer: \(manufacturer)
+        cost in credits: \(costInCredits)
+        length: \(length)
+        max atmosphering speed: \(maxAtmospheringSpeed)
+        hyper dive rating: \(hyperdriveRating)
+        starship class: \(starshipClass)
+        crew: \(crew)
+        passengers: \(passengers)
+        cargo capacity: \(cargoCapacity)
+        """
     }
 }
+
+struct Planets: Decodable {
+    let count: Int
+    let results: [Planet]
+}
+
+struct Planet: Decodable {
+    let name: String
+    let rotationPeriod: String
+    let orbitalPeriod: String
+    let diameter: String
+    let climate: String
+    let gravity: String
+    let surfaceWater: String
+    let population: String
+    let url: URL
+    
+    var description: String {
+        """
+        name: \(name)
+        rotation period: \(rotationPeriod)
+        orbital period: \(orbitalPeriod)
+        diameter: \(diameter)
+        climate: \(climate)
+        gravity: \(gravity)
+        surface water: \(surfaceWater)
+        population: \(population)
+        """
+    }
+}
+
+
